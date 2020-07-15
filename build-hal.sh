1567f168e9fc9b7494b758276230cfb7243c0da6
Darkness Hidden <kmd36rk@gmail.com>
2020-07-15 18:06:49 +0700

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,11 +5,11 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 breakfast $DEVICE
-make mkbootimg
-make hybris-boot
-make halium-boot
-make recoveryimage
-make systemimage 
+mka mkbootimg
+mka hybris-boot
+mka halium-boot
+mka recoveryimage
+mka systemimage 
 
 echo "md5sum halium-boot.img and system.img"
 md5sum $ANDROID_ROOT/out/target/product/land/halium-boot.img
